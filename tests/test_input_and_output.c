41da1ea387d64de19590688d25a51fa073899297
Sumit Das <87431704+codebysumit@users.noreply.github.com>
2025-07-13 01:19:13 +0530

    Update test_input_and_output.c

--- tests/test_input_and_output.c
+++ tests/test_input_and_output.c
@@ -1,53 +1,27 @@
 #include <getstr.h>
-
 #include <stdio.h>
 
 
+int main() {
+    // Get Input With Prompt
+    printf("----Using Prompt----\n");
+    str string = getstrf("Prompt: ");
+    printf("Input: %s\n", string);
 
-int main()
+    // Get Input Without Prompt
+    printf("----Without Prompt----\n");
+    str string2 = getstrf(NULL);
+    printf("Input: %s\n", string2);
 
-{
+    // Using Single Format Specifire
+    printf("----Using Single Format Specifire----\n");
+    str string3 = getstrf("Prompt %d: ", 1);
+    printf("Input: %s\n", string3);
 
-	// Get Input With Prompt
+    // Using Multiple Format Specifire
+    printf("----Using Multiple Format Specifire----\n");
+    str string4 = getstrf("%dst prompt (%s)%c%c", 1, "is", ':', ' ');
+    printf("Input: %s\n", string4);
 
-	printf("----Using Prompt----\n");
-
-	str string = getstrf("Prompt: ");
-
-	printf("Input: %s\n", string);
-
-	
-
-	// Get Input Without Prompt
-
-	printf("----Without Prompt----\n");
-
-	str string2 = getstrf(NULL);
-
-	printf("Input: %s\n", string2);
-
-	
-
-	// Using Single Format Specifire
-
-	printf("----Using Single Format Specifire----\n");
-
-	str string3 = getstrf("Prompt %d: ", 1);
-
-	printf("Input: %s\n", string3);
-
-	
-
-	// Using Multiple Format Specifire
-
-	printf("----Using Multiple Format Specifire----\n");
-
-	str string4 = getstrf("%dst prompt (%s)%c%c", 1, "is", ':', ' ');
-
-	printf("Input: %s\n", string4);
-
-	
-
-	return 0;
-
+    return 0;
 }
